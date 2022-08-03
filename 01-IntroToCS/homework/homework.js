513b02242c63a1144a3d8630a65c65f0baeda34a
unknown <ag.prog.full@outlook.com>
2022-08-03 18:51:55 -0300

    finished homework 01 IntroToCS

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,26 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  
+  var decimal = 0,
+  posicion = 0;
 
+  for (let i = num.length - 1; i >= 0; i--){
+    decimal = decimal + Math.pow(2,posicion) * num[i];
+    posicion++;
+  }
+  return decimal; 
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  
+  var binario = []
+  while(num > 0){
+    binario.unshift(num % 2)
+    num = Math.floor(num / 2)
+  }
+  return binario.join('');
 }
 
 
