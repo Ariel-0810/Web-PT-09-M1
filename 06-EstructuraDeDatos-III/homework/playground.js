02b52bc360b864238271444d70a00d6264e2e1a5
unknown <ag.prog.full@outlook.com>
2022-08-23 20:30:35 -0300

    finished homework 06-EstructuraDeDatos-II

---
+++ 06-EstructuraDeDatos-III/homework/playground.js
@@ -0,0 +1,24 @@
+// const nemo = ['nemo'];
+// const peces = ['walter', 'pinkman', 'tuco', 'donatelo', 'michael', 'jordan', 'peter', 'parker', 'porker', 'dory', 'marlin', 'darla', 'bruce', 'hank', 'nigel', 'chris', 'nemo']
+// const oceano = new Array(100000).fill('beto');
+
+// function encuentraNemo(arr){
+//     let t0 = performance.now();
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] === 'nemo') console.log('ENCONTRE A NEMO!');
+//     }
+//     let t1 = performance.now();
+//     console.log(`Tiempo de ejecucion: ${t1-t0} milisegundos`);
+// }
+
+// encuentraNemo(nemo);
+
+var myArr = [25,26,4,12,5,6,32,15,68,59,45,21,23,5,6,3];
+
+function mybbs(arr){
+
+    return arregloOrdenado;
+}
+
+mybbs(myArr);
+
